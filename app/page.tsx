46d3a7cbd8dfccb475c27aeb2f97fa3a22c9beba
Ricardo Boraure <r.boraure@kation.com.do>
2023-01-14 12:18:27 -0400

    ✨ Add hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Hero } from '@/components/Home/Hero'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main></main>
+    <main>
+      <Hero />
+    </main>
   )
 }
